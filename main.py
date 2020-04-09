c9d403a5083ba5abe7570d9569ddead71a9aac27
Ground Control Audio <info@groundcontrolaudio.com>
2020-04-08 20:26:52 -0400

    added r2 calculations w/ sse, srr and sst

--- main.py
+++ main.py
@@ -30,6 +30,16 @@ class SimpleLinearRegressionClassifier:
         mse = ((y_mse_pred - y_mse) ** 2).mean(axis=0)
         return mse
 
+    # r2 or coefficient of determination - closer to 1.0 means more significance
+    def rsquare(self, x_rsquare, y_actual):
+        y_rsquare_pred = self.predict(x_rsquare)
+        y_actual_mean = np.mean(y_actual)
+        sse = np.sum((y_actual - y_rsquare_pred)**2)        # sum of squared errors, which is the sum of the square of the difference between actual y and the predicted y
+        ssr = np.sum((y_rsquare_pred - y_actual_mean)**2)   # sum of squared residuals, which is the sum of the square of the difference between the predicted y and the actual y mean
+        sst = np.sum((y_actual - y_actual_mean)**2)         # sum of squares total, which is the sum of the square of the difference between the actual y and the actual y mean
+        rsquare = ssr / sst
+        return (rsquare, sse, ssr, sst)
+
 # load de sklearn iris dataset
 data_iris = load_iris()
 X_iris = data_iris.data[:, 0]   # sepal length
@@ -47,7 +57,10 @@ ax.scatter(X_test, y_test)
 ax.plot(X_test, y_pred, color="red")
 
 mse = model.mse(X_test, y_test)
-ax.set_title("Linear regression on iris dataset w/ MSE of " + "%.4f" % mse)
+rsquare, sse, ssr, sst = model.rsquare(X_test, y_test)
+
+ax.set_title("Linear regression on iris dataset w/ MSE %.2f" % mse + ", R2 of %.2f" % rsquare +
+             " (sse: %.2f" % sse + ", ssr: %.2f" % ssr + ", sst: %.2f" % rsquare + ")")
 plt.xlabel('Sepal length')
 plt.ylabel('Sepal width')
 
